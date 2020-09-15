8b93285a4cc060f60095a97bfa7fdbc625118929
Luis Gustavo <lgrotrab@gmail.com>
2020-09-15 13:56:04 -0300

    Updates

---
+++ precache-manifest.f567380a72fca874be07d3c6553e4ef7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3762f15f3129222d132cd344520e949b",
+    "url": "/recface/index.html"
+  },
+  {
+    "revision": "1dbc53784aa7708769f7",
+    "url": "/recface/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d38553e903a1c3da0700",
+    "url": "/recface/static/css/main.ff19a63d.chunk.css"
+  },
+  {
+    "revision": "1dbc53784aa7708769f7",
+    "url": "/recface/static/js/2.8d964b4d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/recface/static/js/2.8d964b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d38553e903a1c3da0700",
+    "url": "/recface/static/js/main.6f8b904e.chunk.js"
+  },
+  {
+    "revision": "9a74b33518a5f36e1b4e",
+    "url": "/recface/static/js/runtime-main.d65f9d43.js"
+  },
+  {
+    "revision": "1f2033840822fecb650c40c20b6005f7",
+    "url": "/recface/static/media/brain.1f203384.jpg"
+  }
+]);
