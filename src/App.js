6510537f73ba6e1e8df27d7b0f99d6146a6e2cf8
Luis Gustavo <lgrotrab@gmail.com>
2020-09-16 14:26:03 -0300

    mudanças na barra de input para torna-la responsiva

--- src/App.js
+++ src/App.js
@@ -1,106 +1,112 @@
-import React, {Component} from 'react';
-import Clarifai from 'clarifai';
-import Navigation from './components/Navigation/Navigation';
-import SignIn from './components/SignIn/SignIn';
-import Register from './components/Register/Register';
-import Logo from './components/Logo/Logo';
-import ImageForm from './components/ImageForm/ImageForm';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
-import 'tachyons';
-import './App.css';
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import ImageForm from "./components/ImageForm/ImageForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+import "tachyons";
+import "./App.css";
 
 const app = new Clarifai.App({
-  apiKey: '4167d68b7c9047cea4efe9b0c85809bd'
- });
+  apiKey: "4167d68b7c9047cea4efe9b0c85809bd",
+});
 
 const particlesOptions = {
-    particles: {
-      number:{
-        value: 50,
-        density: {
-          enable: true,
-          value_area: 800
-        }
-      }
-    }
-}
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
+      input: "",
+      imageUrl: "",
       box: {},
-      route: 'signin',
-      isSignedIn: false
-    }
+      route: "signin",
+      isSignedIn: false,
+    };
   }
 
-  onInputChange = (event) =>{
-    this.setState({input: event.target.value});
-  }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
-      leftCol: clarifaiFace.left_col*width,
-      topRow: clarifaiFace.top_row*height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height) 
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-  }
+    this.setState({ route: route });
+  };
 
-  displayFaceBox = (box) =>{
-
-    this.setState({box: box});
-  }
-
-  onSubmit = () =>{
-    this.setState({imageUrl : this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err => console.log(err));
-  }
-
-  onRouteChange = (route) =>{
-    if(route === 'signout'){
-      this.setState({isSignedIn: false});
-    }else if(route === 'home'){
-      this.setState({isSignedIn: true});
-    }
-    this.setState({route: route});
-  }
-  
-  render(){
-    const {isSignedIn, imageUrl, box, route} = this.state;
-    return (    
+  render() {
+    const { isSignedIn, imageUrl, box, route } = this.state;
+    return (
       <div className="App">
-        <Particles className='particles' params = {particlesOptions}/>
-        <Navigation isSignedIn= {isSignedIn} onRouteChange ={this.onRouteChange}/>
-        { route === 'home' 
-          ?<>
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <>
             <Logo />
             <Rank />
-            <ImageForm onInputChange={this.onInputChange} onButtonSubmit={this.onSubmit}/>
-            <FaceRecognition box={box} imageUrl = {imageUrl}/>
-          </> 
-          
-          :(route === 'signin'
-            ?<SignIn onRouteChange ={this.onRouteChange}/>
-            :<Register onRouteChange = {this.onRouteChange} />
-          )
-        }
+            <ImageForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </>
+        ) : route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/ImageForm/ImageForm.js
+++ src/components/ImageForm/ImageForm.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import './ImageForm.css';
+import React from "react";
+import "./ImageForm.css";
 
-
-const ImageForm = ({onInputChange, onButtonSubmit}) =>{
-    return(
-        <>
-            <p className='f3'>
-                {'Este cerébro mágico irá detectar rostos nas suas imagens, teste a vontade'}
-            </p>
-            <div className='form center pa4 br3 shadow-5'>
-                <input className='f4 p2 w-70 center' type='text' onChange={onInputChange}/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detectar</button>
-            </div>
-        </>
-    );
-}
+const ImageForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <>
+      <p className="f3">
+        {
+          "Este cerébro mágico irá detectar rostos nas suas imagens, teste a vontade"
+        }
+      </p>
+      <div className="form center pa4 br3 shadow-5 w-70">
+        <input
+          className="f4 p2 w-70 center"
+          type="text"
+          onChange={onInputChange}
+        />
+        <button
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          onClick={onButtonSubmit}
+        >
+          Detectar
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default ImageForm;
